86857b8cba0743085878ede5ddb060e8279a8769
Grace de la Mora <grace.g.delamora@gmail.com>
2021-03-17 22:39:22 -0400

    incomplete task 2

--- index.js
+++ index.js
@@ -77,7 +77,27 @@ class Airplane {
   */
   
  class Car {
-    
+    constructor(model, milesPerGallon) {
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+      this.tank = 0;
+      this.odometer = 0;
+    }
+    fill(gallons) {
+      this.tank += gallons;
+    }
+    drive(distance) {
+      let req = distance / this.milesPerGallon;
+      while (req > 0) {
+        this.tank--;
+        this.odometer++;
+        distance--;
+        req /= distance / this.milesPerGallon;
+      }
+      if (this.tank === 0) {
+        return `I ran out of fuel at ${this.odometer} miles!`;
+      }
+    }
   }
   
   /*
